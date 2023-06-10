f976ab052d46edf4c2ad1efe48457a7fb805cde7
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-06-10 13:10:18 +0300

    Initial commit

--- models_demos/web/views.py
+++ models_demos/web/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
-from .models import Employees
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from .models import Employees, Department
 
 
 def index(request):
 
     employees = Employees.objects.all()
-    employees2 = Employees.objects.all()
+    employees2 = Employees.objects.filter()
+
+    # department = Department.objects.get(pk=4)
+    department = get_object_or_404(Department, pk=1)
 
     context = {
         "employees": employees,
         "employees2": employees2,
+        "fun_department": department,
     }
 
     return render(request, 'web/index.html', context)
