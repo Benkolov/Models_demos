7781835bc957aa402970dca25c306f3f43eef42d
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-06-10 13:29:10 +0300

    Initial commit

--- models_demos/web/views.py
+++ models_demos/web/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from .models import Employees, Department
 
 
@@ -17,3 +17,9 @@ def index(request):
     }
 
     return render(request, 'web/index.html', context)
+
+
+def delete_employee(request, pk):
+    employee = get_object_or_404(Employees, pk=pk)
+    employee.delete()
+    return redirect('index')
